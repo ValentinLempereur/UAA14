99e5fdd6f8be715e5f9bcf38e3352625699eda7f
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-09-02 14:35:13 +0200

    truc

---
+++ Exercice_1/MethodeDuProjet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice_1
+{
+    public struct MethodeDuProjet
+    {
+        public double Lecture(string question)
+        {
+            double nbr;
+
+            Console.WriteLine(question);
+            while (!double.TryParse(Console.ReadLine(), out nbr))
+            {
+                Console.WriteLine("Reencoder votre chiffre");
+            }
+
+            return nbr;
+        }
+    }
+}
---
+++ Exercice_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodeDuProjet Outils = new MethodeDuProjet();
+            double[] T = new double[9];
+
+            string question = "encoder un chiffre";
+
+            for (int i = 0; i > 10; i++)
+            {
+                T[i] = Outils.Lecture(question);
+            }
+        }
+    }
+}
