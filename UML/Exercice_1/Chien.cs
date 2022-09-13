8f62c2db1de0b9dbd8a7768b310a7bf7570d0fa0
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-09-13 14:57:40 +0200

    début
    début

---
+++ UML/Exercice_1/Chien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice_1
+{
+    class Chien
+    {
+        // attribut privé par convention
+        private string _nom;
+        private string _race;
+        private int _age;
+
+        //le constructeur
+        public Chien(string nom, string race, int age)
+        {
+            this._nom = nom;
+            this._race = race;
+            this._age = age;
+        }
+
+        //une méthode pour afficher
+        public string AfficheCaracteristique()
+        {
+            string phrase= "Nom : " + this._nom + " - Age : " + this._age + " ans" + "  - Race : " + this._race;
+            return phrase;
+        }
+        
+    }
+}
