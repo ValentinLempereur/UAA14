f978adb1e40dc7ba7cc0d8121db4cdb3dc9fb3af
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-10-11 14:53:07 +0200

    qssq
    sqsq

---
+++ UML/elephant/Elephant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elephant
+{
+    class Elephant
+    {
+        private string _nom;
+        private uint _tailleOreilles;
+
+        public Elephant(string nom, uint taille)
+        {
+            this._nom = nom;
+            this._tailleOreilles = taille;
+        }
+
+
+        public void EcouteMessage(string message, Elephant quiDit)
+        {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+
+        public void EnvoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.EcouteMessage(message, this);
+        }
+
+
+
+
+
+        public string AfficheQuiJeSuis()
+        {
+
+            string phrase = "Il s'appelle " + this._nom + " et sa taille d'oreille fait " + this._tailleOreilles + " cm\n";
+            return phrase;
+        }
+
+    }
+}
---
+++ UML/elephant/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatoire = new Random();
+            int Choix = aleatoire.Next(1, 3);
+
+            string phrase = "";
+            string message = "J'ai vu Dumbo s'envoler devant moi !!";
+
+            Elephant LePremier = new Elephant("Zazou", 150);
+            Elephant LeDeuxieme = new Elephant("Titi", 180);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Taper 1 : Voir les information du premier éléphant\nTaper 2 : Voir les information du deuxième éléphant\nTaper 3 : pour échanger les informations\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int choose = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            switch (choose)
+            {
+                case 1:
+                    phrase = LePremier.AfficheQuiJeSuis();
+                    break;
+                case 2:
+                    phrase = LeDeuxieme.AfficheQuiJeSuis();
+                    break;
+                case 3:
+                    Elephant echange = LePremier;
+                    LePremier= LeDeuxieme;
+                    LeDeuxieme= echange;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nLes informations ont été changé");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+            }
+            Console.WriteLine(phrase);
+
+            switch (Choix)
+            {
+                case 1:
+                    LePremier.EcouteMessage(message, LeDeuxieme);
+                    break;
+                case 2:
+                    LeDeuxieme.EcouteMessage(message, LePremier);
+                    break;
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
