70afaa6d9964abdc991f140f9605c3983b8e62d1
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-11-15 13:52:52 +0100

    suite
    suite

--- WPF/resolution_du_trinome/MainWindow.xaml.cs
+++ WPF/resolution_du_trinome/MainWindow.xaml.cs
@@ -23,16 +23,73 @@ namespace resolution_du_trinome
         public MainWindow()
         {
             InitializeComponent();
+            A.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            B.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            C.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            trynome();
         }
+
+        private void trynome()
+        {
+            int a = A;
+            int b = B;
+            int c = C;
+
+            int delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                -b+Math.Sqrt(delta) / 2*a = int x1;
+            }
+        }
+        private void VerifTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != "," && !EstEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+
+            if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool EstEntier(string texteUser)
+        {
+            bool ok;
+            if (int.TryParse(texteUser, out int text))
+            {
+                ok = true;
+            }
+            else
+            {
+                ok = false;
+            }
+            return ok;
+        }
+
+
 
         private void Button_MouseEnter(object sender, MouseEventArgs e)
         {
-            Button bouton = sender as Button;
+            Result.Background = Brushes.Red;
+            Result.Foreground = Brushes.Black;
         }
 
         private void Button_MouseLeave(object sender, MouseEventArgs e)
         {
+            Result.Background = Brushes.CadetBlue;
+            Result.Foreground = Brushes.CadetBlue;
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            string message = "";
+            page2 essaie = new page2();
+            essaie.TxtResult.Text = message;
+            essaie.Show();
         }
     }
 }
