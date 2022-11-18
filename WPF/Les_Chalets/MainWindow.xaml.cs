1af81909bf8bf28f8e865eebc439e04f4ff89679
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-11-18 14:26:12 +0100

    dfdfd
    fdf

---
+++ WPF/Les_Chalets/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Les_Chalets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string logement;
+        bool reservation = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Prepare();
+        }
+
+        public void Prepare()
+        {
+            
+        }
+
+        private void Radio_Click(object sender, RoutedEventArgs e)
+        {
+            logement = ((RadioButton)sender).Name;
+        }
+
+        private void BoxReservation_Click(object sender, RoutedEventArgs e)
+        {
+            if (reservation == true)
+            {
+                reservation = false;
+            }
+            else 
+            {
+                reservation = true;
+            }
+        }
+    }
+}
