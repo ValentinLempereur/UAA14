8fa3010fdb950109b1b7d05b70b29db90727a910
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-11-22 13:41:55 +0100

    suite
    suite

---
+++ UML/activité 4/Encapsulation/Cercle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    class Cercle
+    {
+        private double _rayon;
+
+        public Cercle(double rayon)
+        {
+            this._rayon = rayon;
+        }
+
+        public double CalculePerimetre()
+        {
+            double value = Math.Round(2 * this._rayon * Math.PI,2);
+            return value;
+        }
+
+        public double CalculeAire()
+        {
+            double value = Math.Round(Math.PI * this._rayon * this._rayon,2);
+            return value;
+        }
+
+        public string AfficheCaracteristique()
+        {
+            string phrase = "le cercle de rayon " + this._rayon;
+            return phrase;
+        }
+
+
+        public double Rayon
+        {
+            get 
+            { 
+                return _rayon; 
+            }
+            set 
+            { 
+                _rayon = value; 
+            }
+        }
+
+    }
+}
---
+++ UML/activité 4/Encapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fin;
+            Console.WriteLine("Bienvenue dans ce programme sur le cercle !\n");
+            do
+            {           
+                Console.WriteLine("Taper un rayon pour votre cercle\n");
+                Cercle mescercles = new Cercle(int.Parse(Console.ReadLine()));
+                Console.Clear();
+
+                double perimetre = mescercles.CalculePerimetre();
+                double aire = mescercles.CalculeAire();
+                string phrase = mescercles.AfficheCaracteristique();
+                Console.WriteLine(phrase + " a un périmètre de " + perimetre + " et une aire de " + aire);
+                Console.WriteLine("----------------------------------------------------------------------");
+                mescercles.Rayon /= 2;
+                Double moitperi = mescercles.CalculePerimetre();
+                double moitaire = mescercles.CalculeAire();
+                string moitphrase = mescercles.AfficheCaracteristique();
+                Console.WriteLine(moitphrase + " a un périmètre de " + moitperi + " et une aire de " + moitaire);
+
+                Console.WriteLine("\nun autre cercle (appuyer sur espace)");
+                fin = Console.ReadLine();
+                Console.Clear();
+            } while (fin == " ");
+        }
+    }
+}
