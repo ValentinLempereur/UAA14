b9a079043348e671a76899e0228f1a50b4521bcd
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-11-22 14:29:43 +0100

    dsd
    ds

---
+++ UML/activité 4/Nombre_Complexe/Complexe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nombre_Complexe
+{
+    class Complexe
+    {
+        private double _NbrReelle;
+        private double _NbrImaginaire;
+
+        public Complexe(double NbrReelle, double NbrImaginaire)
+        {
+            this._NbrReelle = NbrReelle;
+            this._NbrImaginaire = NbrImaginaire;
+        }
+
+        public double  CalculeModule()
+        {
+            double Value = Math.Round(Math.Sqrt(this._NbrReelle * this._NbrReelle + this._NbrImaginaire * this._NbrImaginaire), 2);
+            return Value;
+        }
+
+        public string AfficheCaracteristique(double Value)
+        {
+            string phrase = "complexe vaut : (" + this._NbrReelle + " | " + this._NbrImaginaire + ") a pour module : " + Value;
+            return phrase;
+        }
+
+        public double Reelle
+        {
+            get
+            {
+                return _NbrReelle;
+            }
+            set
+            {
+                _NbrReelle = value;
+            }
+        }
+        public double Imaginaire
+        {
+            get
+            {
+                return _NbrImaginaire;
+            }
+            set
+            {
+                _NbrImaginaire = value;
+            }
+        }
+    }
+}
--- UML/activité 4/Nombre_Complexe/Program.cs
+++ UML/activité 4/Nombre_Complexe/Program.cs
@@ -6,7 +6,28 @@ namespace Nombre_Complexe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Taper un nombre réelle");
+            double reel = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Taper un nombre imaginaire");
+            double ima = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Complexe Nbr1 = new Complexe(reel, ima);
+            double Value = Nbr1.CalculeModule();
+
+            Console.WriteLine("Le Premier " + Nbr1.AfficheCaracteristique(Value));
+
+            Console.WriteLine("\nEncoder un second nombre complexe : \n");
+            Console.WriteLine("Taper un nombre réelle");
+            Nbr1.Reelle = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Taper un nombre imaginaire");
+            Nbr1.Imaginaire = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Le second " + Nbr1.AfficheCaracteristique(Value));
+
+
         }
     }
 }
