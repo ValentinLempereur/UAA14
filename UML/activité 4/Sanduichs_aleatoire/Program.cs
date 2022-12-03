644bf28c9a4b7b34206acf41ece2d12dd1363c66
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2022-12-03 20:46:36 +0100

    bvbyh
    bhvb

---
+++ UML/activité 4/Sanduichs_aleatoire/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sanduichs_aleatoire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SandwichMaker B = new SandwichMaker();
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Bienvenue dans notre concepteur de sandwich !");
+
+            bool ok = true;
+            do
+            {
+                
+                Console.WriteLine("taper sur espace pour générer un sandwich, sinon le concepteur se coupera\n");
+                Console.Clear();
+
+                if (" " == Console.ReadLine())
+                {
+                    Console.WriteLine(B.ComposeSandwich());
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+
+
+            } while (ok);       
+        }
+    }
+}
---
+++ UML/activité 4/Sanduichs_aleatoire/SandwichMaker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sanduichs_aleatoire
+{
+    class SandwichMaker
+    {
+        string[] proteine = new string[4] {"jambon", "fromage", "roast beef", "salami"}; 
+        string[] condiments = new string[4] {"de la mayonnaise", "du ketchup", "de la moutarde", "du tabasco"};
+        string[] pain = new string[3] {"pain blanc", "pain gris", "pain complet"};
+        string[] crudités = new string[4] {"salades", "oeufs", "tomates", "ognons"};
+
+        int[] Nbr1 = new int[3];
+        int Nbr2;
+        Random random = new Random();
+
+        public string ComposeSandwich()
+        {
+
+            Nbr2 = random.Next(3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                
+                Nbr1[i] = random.Next(4);
+            }
+
+            return "Voici votre sandwich : un " + pain[Nbr2] + " avec du " + proteine[Nbr1[0]] + " " + crudités[Nbr1[1]] + " avec " + condiments[Nbr1[2]] + "\n";
+        }
+    }
+}
