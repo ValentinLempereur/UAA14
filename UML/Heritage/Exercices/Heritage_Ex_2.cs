95d07e38ab1bfe03e8d593d7ba23f3b9be5ab64a
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2023-01-24 14:04:40 +0100

    dsds
    dsd

---
+++ UML/Heritage/Exercices/Heritage_Ex_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices
+{
+    internal class Heritage_Ex_2
+    {
+        private int _choix;
+        private string _nom;
+        private Double _date;
+        private Double _puce;
+        private Double _taille;
+        private Double _tailleOreille;
+        private string _concour;
+        private string _aboyer;
+        private string _mialuer;
+        private string _ronronner;
+        private string _bonds;
+        private string _dormir;
+        private string _manger;
+
+        public Heritage_Ex_2(String Nom, Double Date, Double Puce, Double Taille, string Dormir, string Manger, string Concour)
+        {
+            _nom = Nom;
+            _date = Date;
+            _puce = Puce;
+            _taille = Taille;
+            _dormir = Dormir;
+            _manger = Manger;
+            _concour = Concour;
+        }
+
+        public Chien(string Aboyer) : base()
+        {
+            _aboyer = Aboyer;
+        }
+        public Chat(string Miauler, string Ronronner) : base()
+        {
+            _mialuer = Miauler;
+            _ronronner = Ronronner;
+        }
+        public Lapin(double TailleOreille, string Bonds) : base()
+        {
+            _tailleOreille = TailleOreille;
+            _bonds = Bonds;
+        }
+    }
+}
---
+++ UML/Heritage/Exercices/Program.cs
@@ -0,0 +1,6 @@
+﻿using Exercices;
+using System;
+
+Heritage_Ex_2 Page2 = new Heritage_Ex_2();
+
+Console.WriteLine("Veuiller Choisir le type d'animal que vous voulez \nTaper 1 pour le Chien \nTaper 2 pour le Chat \nTaper 2 pour le Lapin");
