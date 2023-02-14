5d7611c99f6689168308b0f785c859e1c3d9acfc
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2023-02-14 14:49:09 +0100

    dsds
    dssd

---
+++ BD/PremiereTestBD/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//ajout pour le DataSet
+using System.Data;
+
+
+namespace PremiereTestBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test Page1 = new Test();
+            DataSet ds;
+            //----------------------------Sélectionné des infos et les affichées----------------------------
+            Console.WriteLine("Teston les accès aux bases de Données !");
+            Console.WriteLine("========================================");
+            Console.WriteLine("Chercher une information particulière ou lister toute la table : \nPar exemple dans la table des clients, faire une recherchesur base du nom de la société");
+            Console.WriteLine("---------------------------------------------------------------------------------------\n");
+            Console.WriteLine("Quelle société ? (par exemple Around the Horn, France restauration ou rien pour la liste complète\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string NomSociete = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (Page1.CreationSelect(NomSociete, out ds))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n" + Page1.AfficheSELECT(ds));
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            //----------------------------Ajouter une ligne dans la base de donnée----------------------------
+            Console.WriteLine("\nAjouter une ligne dans une table : ");
+            Console.WriteLine("Par exemple ajouter une catégorie dans la table des catégories");
+            Console.WriteLine("--------------------------------------------------------------\n");
+            Console.WriteLine("Nom de la catégorie à ajouter");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string NameCategorie = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Description");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string Description = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (Page1.CreationAdd(NameCategorie, Description))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nAjout éffectué");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+
+            //----------------------------Modifier des information dans une base de donnée----------------------------
+            Console.WriteLine("\nNom du messager dont on veut modifier le numéro de téléphone (Speedy Express / united Package / Federal Shipping)");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string NomMessager = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Nouveau numéro de téléphone");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string Numero = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (Page1.CreationUptade(NomMessager, Numero))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nModification éffectué");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BD/PremiereTestBD/Test.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Ajout des using pour la Base de donnée
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+
+namespace PremiereTestBD
+{
+    public struct Test
+    {
+        //Fonction qui permet de Définir la connexion
+        public string DefinirCheminBD()
+        {
+            string CheminBDRelatif = System.IO.Directory.GetCurrentDirectory() + "\\Comptoirs.accdb";
+            return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + CheminBDRelatif;
+        }
+
+
+        public bool CreationSelect(string NomSociete, out DataSet ds)
+        {
+            bool ok = false;
+            //Définir la connexion gràce à une fonction
+            OleDbConnection maConnexion = new OleDbConnection(DefinirCheminBD());
+            //Définir DataSet
+            ds = new DataSet();
+            //définir ma requête + ouvrir la sessions de la base de donnée
+            string query = "";
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                if (NomSociete != "")
+                {
+                    query = "SELECT * FROM Clients WHERE Société=\"" + NomSociete + "\";";
+                }
+                else
+                {
+                    query = "SELECT * FROM Clients";
+                }
+                //on va chercher tout sa dans la bases de données grace au query et la connextion qui permet de rentrer dedans => OleDbDataAdapter
+                OleDbDataAdapter da = new OleDbDataAdapter(query, maConnexion);
+                //mesDonnees ne servent à rien et on met les info dans ds
+                da.Fill(ds, "mesDonnees");
+                //Vérifie si on à bien récupré quelques choses
+                if (ds.Tables[0].Rows.Count >= 1)   
+                {
+                    ok = true;
+                }
+
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+      
+
+            return ok;
+        }
+
+        public string AfficheSELECT(DataSet donnees)
+        {
+            string infos = "";
+            for (int i = 0; i < donnees.Tables[0].Rows.Count; i++)
+            {
+                infos += donnees.Tables[0].Rows[i]["Code client"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["Société"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["Contact"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["Adresse"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["Code postal"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["Ville"].ToString() + "|";
+            }
+            return infos;
+        }
+
+
+
+
+
+
+
+
+        public bool CreationAdd(string NameCategorie, string Description)
+        {
+            bool ok = false;
+            //Définir la connexion gràce à une fonction
+            OleDbConnection maConnexion = new OleDbConnection(DefinirCheminBD());
+
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                //On crée la requête
+                string query = "INSERT INTO Catégories (NomdeCategorie, Description) VALUES(@nomdecatégorie, description);";
+                //on va ajouter la ligne dans la bases de données grace au query et la connextion qui permet de rentrer dedans => OleDbCommand
+                OleDbCommand AjoutDa = new OleDbCommand(query, maConnexion);
+                //on ajoute la ligne et c'est information qui suit 
+                AjoutDa.Parameters.AddWithValue("@nomdecatégorie", NameCategorie);
+                AjoutDa.Parameters.AddWithValue("@description", Description);
+                //on éxécute la comande avec => ExecuteNonQuery() et sa renvoie le nombre de ligne on vérifie donc si il y en a au moins 1 ou plus éffectué
+                if (AjoutDa.ExecuteNonQuery() >= 1)
+                {
+                    ok = true;
+                }
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+
+
+        public bool CreationUptade(string NomMessager, string Numero)
+        {
+            bool ok = false;
+
+            //Définir la connexion gràce à une fonction
+            OleDbConnection maConnexion = new OleDbConnection(DefinirCheminBD());
+
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                //On crée la requête
+                string query = "UPDATE Messagers SET Telephone=@paramètre WHERE NomDuMessager =" + NomMessager + ";";
+                //on va ajouter la ligne dans la bases de données grace au query et la connextion qui permet de rentrer dedans => OleDbCommand
+                OleDbCommand UptadeDa = new OleDbCommand(query, maConnexion);
+                //on modifie la ligne 
+                UptadeDa.Parameters.AddWithValue("@paramètre", Numero);
+                //on éxécute la comande avec => ExecuteNonQuery() et sa renvoie le nombre de ligne on vérifie donc si il y en a au moins 1 ou plus éffectué
+                if (UptadeDa.ExecuteNonQuery() >= 1)
+                {
+                    ok = true;
+                }
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+    }
+}
