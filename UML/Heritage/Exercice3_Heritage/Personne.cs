2c9a491e9c949436abdc601bbd1a459141e3d629
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2023-02-16 15:28:24 +0100

    dsds
    dsd

---
+++ UML/Heritage/Exercice3_Heritage/Personne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice3_Heritage
+{
+    class Personne
+    {
+        protected int _matricule;
+        protected string _nom;
+        protected string _prenom;
+        protected int _dateNaissance;
+    }
+}
