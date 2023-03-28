1b7c21f523abd3b335cc8d1c51ec1dc738aa4686
Lempereur Valentin <160646@CCM-Asty-Moulin.local>
2023-03-28 14:48:32 +0200

    truc

---
+++ BD/FirstMySQL/MySQL.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Ajout des using pour la Base de donnée
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Collections;
+
+namespace FirstMySQL
+{
+    class MySQL
+    {
+        public string DefinirCheminBD()
+        {
+            return "Server=localhost;Database=firstmysql;port=3306;User Id=root;password=root";
+        }
+
+        public bool CreationSelect(string Nom, out DataSet ds)
+        {
+            bool ok = false;
+            //Définir la connexion gràce à une fonction
+            MySqlConnection maConnexion = new MySqlConnection(DefinirCheminBD());
+            //Définir DataSet
+            ds = new DataSet();
+            //définir ma requête + ouvrir la sessions de la base de donnée
+            string query = "";
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                if (Nom != "")
+                {
+                    query = "SELECT * FROM utilisateurs WHERE nomUser=\"" + Nom + "\"";
+                }
+                else
+                {
+                    query = "SELECT * FROM utilisateurs";
+                }
+                //on va chercher tout sa dans la bases de données grace au query et la connextion qui permet de rentrer dedans => MySqlDataAdapter
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnexion);
+                //mesDonnees ne servent à rien et on met les info dans ds
+                da.Fill(ds, "mesDonnees");
+                //Vérifie si on à bien récupré quelques choses
+                if (ds.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+
+
+
+
+        public string AfficheSELECT(DataSet donnees)
+        {
+            string infos = "";
+            for (int i = 0; i < donnees.Tables[0].Rows.Count; i++)
+            {
+                infos += donnees.Tables[0].Rows[i]["nomUser"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["prenomUser"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["loginUser"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["passWorduser"].ToString() + "|" +
+                         donnees.Tables[0].Rows[i]["role"].ToString() + "\n";
+            }
+            return infos;
+        }
+
+
+
+        public bool CreationAdd(string Name, string Chemin)
+        {
+            bool ok = false;
+            //Définir la connexion gràce à une fonction
+            MySqlConnection maConnexion = new MySqlConnection(DefinirCheminBD());
+
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                //On crée la requête
+                string query = "INSERT INTO images (name, image) VALUES(@name, @image)";
+                //on va ajouter la ligne dans la bases de données grace au query et la connextion qui permet de rentrer dedans => MySqlCommand
+                MySqlCommand AjoutDa = new MySqlCommand(query, maConnexion);
+                //on ajoute la ligne et c'est information qui suit 
+                AjoutDa.Parameters.AddWithValue("@name", Name);
+                AjoutDa.Parameters.AddWithValue("@image", "images/DB/" + Chemin + ".jpg");
+                //on éxécute la comande avec => ExecuteNonQuery() et sa renvoie le nombre de ligne on vérifie donc si il y en a au moins 1 ou plus éffectué
+                if (AjoutDa.ExecuteNonQuery() >= 1)
+                {
+                    ok = true;
+                }
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+
+
+        public bool CreationUptade(string Name, string colonne, string infochanger)
+        {
+            bool ok = false;
+
+            //Définir la connexion gràce à une fonction
+            MySqlConnection maConnexion = new MySqlConnection(DefinirCheminBD());
+
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                //On crée la requête
+                string query = "UPDATE utilisateurs SET " + colonne + "=@parametre WHERE nomUser =\"" + Name + "\"";
+                //on va ajouter la ligne dans la bases de données grace au query et la connextion qui permet de rentrer dedans => MySqlCommand 
+                MySqlCommand UptadeDa = new MySqlCommand(query, maConnexion);
+                //on modifie la ligne 
+                UptadeDa.Parameters.AddWithValue("@parametre", infochanger);
+                //on éxécute la comande avec => ExecuteNonQuery() et sa renvoie le nombre de ligne on vérifie donc si il y en a au moins 1 ou plus éffectué
+                if (UptadeDa.ExecuteNonQuery() >= 1)
+                {
+                    ok = true;
+                }
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+
+
+        public bool CreationListe(out DataSet ds)
+        {
+            bool ok = false;
+            //Définir la connexion gràce à une fonction
+            MySqlConnection maConnexion = new MySqlConnection(DefinirCheminBD());
+            //Définir DataSet
+            ds = new DataSet();
+            //définir ma requête + ouvrir la sessions de la base de donnée
+            string query = "";
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                //Création de la requête
+                query = "SELECT * FROM utilisateurs";
+                //on va chercher tout sa dans la bases de données grace au query et la connextion qui permet de rentrer dedans => MySqlDataAdapter
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnexion);
+                //mesDonnees ne servent à rien et on met les info dans ds
+                da.Fill(ds, "mesDonnees");
+                //Vérifie si on à bien récupré quelques choses
+                if (ds.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+
+
+
+        public bool CreationDelete(string LeChamp)
+        {
+            bool ok = false;
+            //Définir la connexion gràce à une fonction
+            MySqlConnection maConnexion = new MySqlConnection(DefinirCheminBD());
+
+            try
+            {
+                //on ouvre la connexion
+                maConnexion.Open();
+                //On crée la requête
+                string query = "DELETE FROM utilisateurs WHERE nomUser='" + LeChamp + "'";
+                //on va ajouter la ligne dans la bases de données grace au query et la connextion qui permet de rentrer dedans => MySqlCommand
+                MySqlCommand DeleteDa = new MySqlCommand(query, maConnexion);
+                //on éxécute la comande avec => ExecuteNonQuery() et sa renvoie le nombre de ligne on vérifie donc si il y en a au moins 1 ou plus éffectué
+                if (DeleteDa.ExecuteNonQuery() >= 1)
+                {
+                    ok = true;
+                }
+                //on ferme la connexion le plus vote possible 
+                maConnexion.Close();
+            }
+            //si sa ne marche pas, permet de ne pas planter et de mettre un message d'erreur
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+    }
+}
---
+++ BD/FirstMySQL/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace FirstMySQL
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MySQL mysql = new MySQL();
+            DataSet ds;
+
+            //----------------------------Sélectionné des infos et les affichées----------------------------
+            Console.WriteLine("Teston les accès aux bases de Données !");
+            Console.WriteLine("========================================");
+            Console.WriteLine("Chercher une information particulière ou lister toute la table : \nPar exemple dans la table des utilisateur, faire une recherchesur base du nom de la société");
+            Console.WriteLine("---------------------------------------------------------------------------------------\n");
+            Console.WriteLine("quelle personne voulez-vous savoir c'est information (nom) ? (par exemple haddock, ben ou rien pour la liste complète\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string nom = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            test(nom);
+
+
+            //----------------------------Ajouter une ligne dans la base de donnée----------------------------
+            Console.WriteLine("\nAjouter une ligne dans une table : ");
+            Console.WriteLine("Par exemple ajouter une image dans la table des images");
+            Console.WriteLine("--------------------------------------------------------------\n");
+            Console.WriteLine("Nom de l'image à ajouter");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string Name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("nom du pour le liens d'image");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string Chemin = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (mysql.CreationAdd(Name, Chemin))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nAjout éffectué");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            //----------------------------Modifier des information dans une base de donnée----------------------------
+            nom = "";
+            Console.WriteLine("\nEntrez le nom d'une personne pour voir c'est information et ansi changer une information");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            nom = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("nomUser | prenomUser | loginUser | passWordUser | role");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            test(nom);
+            if (nom == "")
+            {
+                Console.WriteLine("\nEntrez le nom de la personne que vous voulez modifier");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                nom = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            
+            Console.WriteLine("\nQuelle colonne voulez vous changer");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string colonne = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nEntrer la nouvelle information");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string infochanger = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (mysql.CreationUptade(nom, colonne, infochanger))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nModification éffectué");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            
+            //----------------------------Listez tous les éléments d'un champ d'une table dans une base de donnée----------------------------
+            Console.WriteLine("\nListe des catégories disponibles : ");
+            if (mysql.CreationListe(out ds))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(mysql.AfficheSELECT(ds));
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            
+
+
+            //----------------------------Supprimer une ligne dans une base de donnée----------------------------
+            Console.WriteLine("Supprimer une ligne dans la table de utilisateurs");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Séléctionné la ligne");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            String LeChamp = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            if (mysql.CreationDelete(LeChamp))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("La ligne à été suprrimer");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.ReadLine();
+        }
+
+        static void test(string nom)
+        {
+            DataSet ds;
+            MySQL mysql = new MySQL();
+            
+            if (mysql.CreationSelect(nom, out ds))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n" + mysql.AfficheSELECT(ds));
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sa ne marche pas");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
